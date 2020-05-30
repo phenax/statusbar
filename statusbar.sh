a9b93021b54c732b2df49ba769c2a4beed6af86a
Akshay Nair <phenax5@gmail.com>
2020-05-30 19:26:00 +0530

    Got rid of the fancy spancy ps code

--- statusbar.sh
+++ statusbar.sh
@@ -4,12 +4,8 @@ export ROOT=$HOME/scripts/statusbar;
 source $ROOT/config.sh;
 source $ROOT/helpers.sh;
 
-TMP_PID_FILE=/tmp/status-monkeybar.pid;
-
 reset_bar() {
   killall -9 lemonbar;
-  [[ -f $TMP_PID_FILE ]] && (cat $TMP_PID_FILE | xargs kill);
-  rm $TMP_PID_FILE;
 
   bspc config top_padding 0;
   bspc config bottom_padding 0;
@@ -29,9 +25,7 @@ start_bar() {
 case "$1" in
   show)
     reset_bar 2> /dev/null;
-    start_bar 2> /dev/null &
-    echo -e $! > $TMP_PID_FILE;
-    disown;
+    start_bar 2> /dev/null;
   ;;
   hide) reset_bar 2> /dev/null; ;;
   *) echo "Usage: statusbar.sh (show|hide)" ;;
